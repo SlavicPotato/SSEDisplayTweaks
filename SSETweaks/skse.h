c521d85f4812dcf3da49cf86612c8fa7951cb530
SlavicPotato <sp@eth.si>
2021-04-17 09:10:08 +0200

    Reserve more space for the trampoline

--- SSETweaks/skse.h
+++ SSETweaks/skse.h
@@ -3,7 +3,7 @@
 namespace SDT
 {
     static inline constexpr std::size_t MAX_TRAMPOLINE_BRANCH = 256;
-    static inline constexpr std::size_t MAX_TRAMPOLINE_CODEGEN = 384;
+    static inline constexpr std::size_t MAX_TRAMPOLINE_CODEGEN = 512;
 
     class ISKSE
     {
